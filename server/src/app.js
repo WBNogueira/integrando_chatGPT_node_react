3fe91580c560ce22ad5dab69a5a7bdce11d33a09
Wagner Nogueira <wbnogueira3@gmail.com>
2023-12-07 11:39:52 -0300

    Criando os arquivos de models, controller e routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+const routes = require("./routes/routes")
 
 require("dotenv").config()
 
@@ -7,5 +8,6 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(routes)
 
 module.exports = app
---
+++ server/src/controllers/prompt-controller.js
@@ -0,0 +1,26 @@
+const InputPrompt = require('../models/input-prompt')
+const openai = require('../config/openai')
+
+module.exports = {
+    async sendText(req, res) { 
+        const openaiAPI = openai.configuration()
+        const inputModel = new InputPrompt(req.body)
+
+        try {
+            const response = await openaiAPI.createCompletion(
+                openai.textCompletion(inputModel)
+            )
+
+            return res.status(200).json({
+                sucess: true,
+                data: response.data.choices[0].text
+            })
+
+        } catch (error) {
+            return res.status(400).json({
+                sucess: false,
+                error: error.response ? error.response.data : "There was an inssue on the server"  
+            })
+        }
+    }
+}
